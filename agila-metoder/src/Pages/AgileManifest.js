114cbdf67c08af191f4f05b77c1caa92a6183c86
brittatoresson <britta_toresson@hotmail.com>
2022-04-25 11:02:11 +0200

    update data agileManifest #17

--- agila-metoder/src/Pages/AgileManifest.js
+++ agila-metoder/src/Pages/AgileManifest.js
@@ -1,8 +1,18 @@
-import { manifest } from "../Components/data";
+// import { manifest } from "../Components/data";
 import Footer from "../Components/Footer";
-import Navbar from "../Components/TopNav";
+import { useState, useEffect } from "react";
 
 function Manifest() {
+  const [state, setState] = useState("");
+  const url = "http://localhost:5002/infoData.json";
+
+  //Fetcha data
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setState(data));
+  }, []);
+
   return (
     <section id="manifest" className="scroller">
       <h3>
@@ -12,17 +22,20 @@ function Manifest() {
         förändring framför att strikt följa en uppgjord plan.
       </h3>
 
-      {manifest.map((element, i) => (
-        <p key={i}>
-          {element.id > 0 ? (
-            element.id + ": " + element.item
-          ) : (
-            <h4>
-              Det agila manifestet beskriver 12 grundprinciper att arbeta efter:
-            </h4>
-          )}
-        </p>
-      ))}
+      {state
+        ? state.manifest.map((element, i) => (
+            <p key={i}>
+              {element.id > 0 ? (
+                element.id + ": " + element.item
+              ) : (
+                <h4>
+                  Det agila manifestet beskriver 12 grundprinciper att arbeta
+                  efter:
+                </h4>
+              )}
+            </p>
+          ))
+        : null}
       <Footer />
     </section>
   );
