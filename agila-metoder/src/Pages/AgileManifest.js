35acae0b9acc0aa917794793cd9a1d9d9abf6713
brittatoresson <britta_toresson@hotmail.com>
2022-04-25 10:47:24 +0200

    uopdate backend #10

--- agila-metoder/src/Pages/AgileManifest.js
+++ agila-metoder/src/Pages/AgileManifest.js
@@ -5,11 +5,22 @@ import Navbar from "../Components/TopNav";
 function Manifest() {
   return (
     <section id="manifest" className="scroller">
-      <Navbar />
+      <h3>
+        Det agila arbetssättet vilar på 4 grunder: individer och interaktiv
+        framför processer, fungerande mjukvara framför omfattande dokumentation,
+        samarbete med kunden framför att förhandla om kontrakt, reagera på
+        förändring framför att strikt följa en uppgjord plan.
+      </h3>
 
       {manifest.map((element, i) => (
         <p key={i}>
-          {element.id > 0 ? element.id + ": " + element.item : element.item}
+          {element.id > 0 ? (
+            element.id + ": " + element.item
+          ) : (
+            <h4>
+              Det agila manifestet beskriver 12 grundprinciper att arbeta efter:
+            </h4>
+          )}
         </p>
       ))}
       <Footer />
